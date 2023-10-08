b52c516e3efd20d5f7e0f134a87fc65d476b840c
Kooreis <kooreis.official@gmail.com>
2023-10-07 19:40:28 -0500

    Added a while loop that continues as long as the start index is less than or equal to the end index. Inside the loop, calculated the middle index of the array.

--- solution.ts
+++ solution.ts
@@ -2,5 +2,8 @@ class RotatedArraySearch {
     public static search(nums: number[], target: number): number {
         let start = 0;
         let end = nums.length - 1;
+        while (start <= end) {
+            let mid = Math.floor(start + (end - start) / 2);
+        }
     }
 }
