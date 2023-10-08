6601c7e67786e56aee82d12144fb14880b08100e
Kooreis <kooreis.official@gmail.com>
2023-10-07 19:40:28 -0500

    Starts a while loop that continues as long as 'start' is less than or equal to 'end'. It calculates the middle index of the current search range and checks if the number at this index is the target. If it is, the function returns this index.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,5 @@
-function search(nums, target) {
-    let start = 0;
-    let end = nums.length - 1;
-}
+while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
